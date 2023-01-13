5c7faab3b3aee83411453019fe609a70823a38a0
Andy Peterson <andy@ndpsoftware.com>
2023-01-13 11:14:22 -0800

    more playing

--- src/regex.ts
+++ src/regex.ts
@@ -1,17 +1,21 @@
-type ExtractGroupNames<T> =
-  T extends `${string}(?<${infer Name}>${infer Rest}` ?
-  (Record<Name, string> & ExtractGroupNames<Rest>) :
-  T extends `(?<${infer Name}>` ? Record<Name, string> : Record<never, any>
+const proto1 = new RegExp('^(?<protocol>http|https|ftp|mailto).*')
 
-interface RegExpMatchedGroups<T> extends RegExpExecArray {
-  groups: ExtractGroupNames<T>
-}
+
+    type ExtractGroupNames<S extends string> =
+      S extends `${string}(?<${infer Name}>${infer Rest}`
+        ? (Record<Name, string> & ExtractGroupNames<Rest>)
+        : Record<never, any>
+
+    interface RegExpMatchedGroups<T> extends RegExpExecArray {
+      groups?: ExtractGroupNames<T>
+    }
 
 interface RegexWithNamedGroup<S extends string> extends RegExp {
   exec (s: string): RegExpMatchedGroups<S>
 }
 
-type StringWithNamedGroup = `${string}(?<${string}>${string}`
+type StringWithNamedGroup = string
+// type StringWithNamedGroup = `${string}(?<${string}>${string}`
 
 interface RegExpConstructor {
   new<T extends StringWithNamedGroup> (pattern: T, flags?: string): RegexWithNamedGroup<T>;
@@ -21,10 +25,14 @@ interface RegExpConstructor {
 
 const nameroo = new RegExp('a(?<nameroo>.*)z')
 
-let r = nameroo.exec('hello')?.groups.nameroo
+let r = nameroo.exec('hello').groups?.nameroo
 
 const foobar = new RegExp('(?<foo>.*):(?<bar>.*)')
 
-console.log(foobar.exec('andy:horse').groups.foo)
-console.log(foobar.exec('andy:horse').groups.bar)
+console.log(foobar.exec('a').groups?.foo)
+console.log(foobar.exec('andy:horse').groups?.bar)
 console.log(foobar.exec('andy:horse').groups)
+
+const proto = new RegExp('^(?<protocol>http|https|ftp|mailto).*')
+console.log(proto.exec('http://example.co').groups?.protocall)
+
