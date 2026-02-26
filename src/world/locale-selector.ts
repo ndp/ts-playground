dfbe471703571220cc92106168876752bb4ecebd
Andy Peterson <andy@ndpsoftware.com>
2026-02-26 11:15:21 -0800

    remove extraneous ! and ? modifiers

--- src/world/locale-selector.ts
+++ src/world/locale-selector.ts
@@ -16,25 +16,25 @@ const LocaleSelector = (new ComponentBwilder())
     console.log(` got country locales for ${newValue}:`, countryLocales)
     if (!countryLocales || countryLocales.length === 0) return
 
-    const isLocked = this.subElements!.segmentedButtons!.hasAttribute('locked')
+    const isLocked = this.subElements.segmentedButtons!.hasAttribute('locked')
     if (isLocked) {
-      const lockedLocale = this.subElements!.segmentedButtons!.getAttribute('data-locked')
+      const lockedLocale = this.subElements.segmentedButtons!.getAttribute('data-locked')
       console.log(`marking locked locale ${lockedLocale} as used in gLanguages`)
       gLanguages.use(lockedLocale!)
     }
     gLanguages.add(...countryLocales)
-    this.subElements!.segmentedButtons!.setAttribute('suggested', countryLocales.join(','))
+    this.subElements.segmentedButtons!.setAttribute('suggested', countryLocales.join(','))
 
     if (countryLocales.length === 0 || isLocked) return
 
-    this.subElements!.segmentedButtons!.setAttribute('data-value', countryLocales[0])
+    this.subElements.segmentedButtons!.setAttribute('data-value', countryLocales[0])
     this.root.dispatchEvent(new CustomEvent('change', {bubbles: true, detail: {value: countryLocales[0]}}))
 
   })
   .wRender(function () {
     console.log('rendering locale buttons with  languages:', gLanguages.peek(), this)
 
-    let segmentedButtons = this.subElements!.segmentedButtons
+    let segmentedButtons = this.subElements.segmentedButtons
     if (!segmentedButtons) {
       segmentedButtons = document.createElement('segmented-buttons')
       segmentedButtons.setAttribute('lockable', '')
